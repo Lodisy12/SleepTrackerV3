9f310773eaccd2c543ec004156eede29d96c4693
Joey Lawter <42898462+Lodisy12@users.noreply.github.com>
2019-11-15 13:14:35 -0800

    Changed the part of the onBindViewHolder class into a function and encapsulated it inside the ViewHolder class. This makes it so that we can add additional view holders

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -16,16 +16,15 @@
 
 package com.example.android.trackmysleepquality.sleeptracker
 
+import android.content.res.Resources
 import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.trackmysleepquality.R
-import com.example.android.trackmysleepquality.TextItemViewHolder
 import com.example.android.trackmysleepquality.convertDurationToFormatted
 import com.example.android.trackmysleepquality.convertNumericQualityToString
 import com.example.android.trackmysleepquality.database.SleepNight
@@ -41,45 +40,49 @@ class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
-        val res = holder.itemView.context.resources
-        holder.sleepLength.text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
-        holder.quality.text = convertNumericQualityToString(item.sleepQuality, res)
 
-        holder.qualityImage.setImageResource(when (item.sleepQuality) {
-            0 -> R.drawable.ic_sleep_0
-            1 -> R.drawable.ic_sleep_1
-            2 -> R.drawable.ic_sleep_2
-            3 -> R.drawable.ic_sleep_3
-            4 -> R.drawable.ic_sleep_4
-            5 -> R.drawable.ic_sleep_5
-            else -> R.drawable.ic_sleep_active
-        })
-
-        holder.sleepLength.setTextColor(when (item.sleepQuality) {
-            0 -> Color.parseColor("#fa0000")
-            1 -> Color.parseColor("#f04d96")
-            2 -> Color.parseColor("#0cb7eb")
-            3 -> Color.parseColor("#0e7796")
-            4 -> Color.parseColor("#14e375")
-            5 -> Color.parseColor("#37fa92")
-            else -> Color.GRAY
-        })
-
-        holder.quality.setTextColor(when (item.sleepQuality) {
-            0 -> Color.parseColor("#fa0000")
-            1 -> Color.parseColor("#f04d96")
-            2 -> Color.parseColor("#0cb7eb")
-            3 -> Color.parseColor("#0e7796")
-            4 -> Color.parseColor("#14e375")
-            5 -> Color.parseColor("#37fa92")
-            else -> Color.GRAY
-        })
+        holder.bind(item)
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         val sleepLength: TextView = itemView.findViewById(R.id.sleep_length)
         val quality: TextView = itemView.findViewById(R.id.quality_string)
         val qualityImage: ImageView = itemView.findViewById(R.id.quality_image)
+        fun bind(item: SleepNight) {
+            val res = itemView.context.resources
+            sleepLength.text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
+            quality.text = convertNumericQualityToString(item.sleepQuality, res)
+
+            qualityImage.setImageResource(when (item.sleepQuality) {
+                0 -> R.drawable.ic_sleep_0
+                1 -> R.drawable.ic_sleep_1
+                2 -> R.drawable.ic_sleep_2
+                3 -> R.drawable.ic_sleep_3
+                4 -> R.drawable.ic_sleep_4
+                5 -> R.drawable.ic_sleep_5
+                else -> R.drawable.ic_sleep_active
+            })
+
+            sleepLength.setTextColor(when (item.sleepQuality) {
+                0 -> Color.parseColor("#fa0000")
+                1 -> Color.parseColor("#f04d96")
+                2 -> Color.parseColor("#0cb7eb")
+                3 -> Color.parseColor("#0e7796")
+                4 -> Color.parseColor("#14e375")
+                5 -> Color.parseColor("#37fa92")
+                else -> Color.GRAY
+            })
+
+            quality.setTextColor(when (item.sleepQuality) {
+                0 -> Color.parseColor("#fa0000")
+                1 -> Color.parseColor("#f04d96")
+                2 -> Color.parseColor("#0cb7eb")
+                3 -> Color.parseColor("#0e7796")
+                4 -> Color.parseColor("#14e375")
+                5 -> Color.parseColor("#37fa92")
+                else -> Color.GRAY
+            })
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
