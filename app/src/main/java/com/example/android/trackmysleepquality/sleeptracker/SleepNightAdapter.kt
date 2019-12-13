5ad613ba8c8de27da4aa6838c762ab6b53f77772
Joey Lawter <42898462+Lodisy12@users.noreply.github.com>
2019-12-12 20:39:53 -0800

    Changed binding and adapter to use DataItems

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -90,7 +90,7 @@ class SleepNightAdapter(val clickListener: SleepNightListener) : ListAdapter<Dat
 
     class ViewHolder private constructor(val binding: ListItemSleepNightBinding) : RecyclerView.ViewHolder(binding.root){
 
-        fun bind(clickListener: SleepNightListener, item: SleepNight) {
+        fun bind(item: SleepNight, clickListener: SleepNightListener) {
             binding.sleep = item
             binding.clickListener = clickListener
             binding.executePendingBindings()
